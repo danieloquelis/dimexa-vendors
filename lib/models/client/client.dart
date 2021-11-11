10ead042c84b59db38b596766565beb6ee3d2b6e
danieloquelis <doquelis@znerg.com>
2021-11-11 23:35:15 +0100

    taking out hive and add sqflite

--- lib/models/client/client.dart
+++ lib/models/client/client.dart
@@ -1,14 +1,8 @@
-import 'package:hive_flutter/hive_flutter.dart';
 
-//part 'client.g.dart';
 
-@HiveType(typeId : 0, adapterName: 'ClientAdapter')
-class Client extends HiveObject {
+class Client {
 
-  @HiveField(0)
   String? id;
-
-  @HiveField(1)
   String? commercialName;
 
   String? socialName;
