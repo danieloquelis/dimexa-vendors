7d3ca97cd72bb505f5fe080d29eb027925c41496
danieloquelis <doquelis@znerg.com>
2021-12-14 19:43:43 +0100

    updating environment enums

--- lib/data/enums/env/env.dart
+++ lib/data/enums/env/env.dart
@@ -8,8 +8,8 @@ extension CatExtension on Env {
   String get url {
     return [
       'http://190.117.109.188:3000',//'http://190.117.166.223:3000',
-      'https://stage.blaze.me',
-      'https://prod.blaze.me'
+      'http://190.117.109.188:3000',
+      'http://190.117.109.188:3000'
     ][index];
   }
 }
